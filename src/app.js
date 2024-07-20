8bcd1492a56e29910ba6a1d2f55cdbe352b4e1d0
Nahuel Silvera <71460850+MrTester007@users.noreply.github.com>
2024-07-20 00:45:44 +0000

    Exercise done

--- src/app.js
+++ src/app.js
@@ -1,11 +1,38 @@
-/* eslint-disable */
 import "bootstrap";
 import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+function getRandomElement(arr) {
+  return arr[Math.floor(Math.random() * arr.length)];
+}
+
+function generateExcuse() {
+  let who = [
+    "The dog",
+    "My grandma",
+    "The mailman",
+    "My bird",
+    "My cat",
+    "My girlfriend"
+  ];
+  let action = ["ate", "peed", "crushed", "broke", "punch"];
+  let what = ["my homework", "my phone", "the car", "my leg"];
+  let when = [
+    "before the class",
+    "when I was sleeping",
+    "while I was exercising",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  let excuse = `${getRandomElement(who)} ${getRandomElement(
+    action
+  )} ${getRandomElement(what)} ${getRandomElement(when)}.`;
+  return excuse;
+}
+
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  document.getElementById("excuse").innerHTML = generateExcuse();
 };
